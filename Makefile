1e105a9d2686721d5ea45d06c64ea1a5749cad0e
mvanbraeckel <mvanbrae@uoguelph.ca>
2019-03-11 19:20:04 -0600

    automatic backup via makefile

---
+++ Makefile
@@ -0,0 +1,27 @@
+# Mitchell Van Braeckel (mvanbrae@uoguelph.ca) 1002297
+# 11/03/2019
+# CIS*3110: Operating Systems A3 - CPU Scheduling: Simple Dispatcher
+# --> Simulates a simple dispacter of an OS using simulated events to do CPU scheduling
+ 
+CC = gcc
+CFLAGS = -g -Wall -std=c99
+
+all: idispatcher
+
+idispatcher: idispatcher.c
+	$(CC) $(CFLAGS) idispatcher.c -o idispatcher
+
+git: *.c Makefile 
+	git add Makefile
+	git add test_inputs
+	git add test_outputs
+	git add *.c
+	git commit -m "automatic backup via makefile"
+	git remote rm origin
+	git config credential.helper store
+	git config --global credential.helper 'cache --timeout 3600'
+	git remote add origin https://github.com/mvanbraeckel/CPUscheduling_3110.git
+	git push -u origin master
+
+clean:
+	rm -f *.o idispatcher
---
+++ idispatcher.c
@@ -0,0 +1,60 @@
+/**
+ * Mitchell Van Braeckel (mvanbrae@uoguelph.ca) 1002297
+ * 11/03/2019
+ * CIS*3110: Operating Systems A3 - CPU Scheduling: Simple Dispatcher
+ * --> Simulates a simple dispacter of an OS using simulated events to do CPU scheduling
+ * 
+ *  (1) The sequence of events will be given in the standard input (one event per line).
+ *  --> Empty line will signify the end of the input.
+ *  --> Format of one line is as follows:
+ *         <time> <event> {<process id>} ; where:
+ *             - <time> - is an integer number denoting local time in milliseconds measured from 0 (time will be strictly increasing from line to line)
+ *             - <event> - is one of the following:
+ *                 – C - create 
+ *                 – E - exit 
+ *                 – R N - request resource number N 
+ *                 – I N - interrupt from resource number N (request accomplished)
+ *                 – T - timer interrupt
+ *             - <process id> - is a nonnegative integer, unique for each process (system idle process has <process id> = 0);
+ * 
+ *  (2) Your Dispatcher will have to keep track of events and changes in the state of the processes, taking into account the following additional conditions:
+ *      1. There are 5 different kinds of resources in the system and requests can be serviced out of order of arrival
+ *      2. Time sharing - the process which is in running state is to be preempted as the result of the timer interrupt if there are other ready processes in the system
+ *      3. Running process can also exit or get blocked because of request for a resource
+ *      4. If there are no ready user processes, then process number 0 (system idle process) is running
+ *      5. If process 0 is running and new process is created, or as the result of an event one of the blocked processes becomes ready (unblocked), this process will get CPU immediately
+ * 
+ *  (3) When all lines of the input are processed, Dispatcher will print the following cumulative information about all processes admitted to the system during simulation:
+ *      <process id> <total time Running> <total time Ready> <total time Blocked>
+ *  --> For the system idle process print only <total time Running> and assume that process 0 was created at time 0.
+ *  --> The output is one line per process in the increasing order of process IDs.
+ * 
+ * You can assume that the sequence of events given in the input is consistent and no input errors are present.
+ */
+
+// =================================== INCLUDES ===================================
+//#include <fcntl.h> // open
+//#include <unistd.h> // read
+//#include <sys/types.h> // read
+//#include <sys/uio.h> // read
+#include <stdio.h> // fopen, fread
+#include <stdlib.h>
+//#include <sys/time.h> // gettimeofday
+//#include <string.h>
+//#include <ctype.h>
+
+// =================================== STRUCTS ====================================
+
+// ============================== FUNCTION PROTOTYPES =============================
+
+// ================================================================================
+
+int main( int argc, char *argv[] ) {
+    // declare variables
+
+
+
+    return 0;
+}
+
+// ================================================================================
